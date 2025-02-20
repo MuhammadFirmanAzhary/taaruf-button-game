9f4a9612f2f7a64be81779973dd3ec1f80c21d1a
muhammadfirmanazhary <muhammadfirmanazhary.com>
2025-02-20 13:30:36 +0700

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+
+function showMessage() {
+    let messages = [
+        "Wah, kamu beneran orang spesial! 😍",
+        "Pilihan terbaik! Kita cocok banget nih! 💖",
+        "Subhanallah, akhirnya aku menemukanmu! 💕",
+        "Ya Allah, hati ini bergetar karena kamu! 😘",
+        "MashaAllah, jodoh itu dekat kalau hati kita sama-sama ikhlas! 💞"
+    ];
+    let randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    document.getElementById("message").innerText = randomMessage;
+    document.getElementById("message").style.display = "block";
+}
+
+let noButton = document.getElementById("noButton");
+
+noButton.addEventListener("mouseover", function() {
+    let moveX = (Math.random() - 0.5) * 400;
+    let moveY = (Math.random() - 0.5) * 200;
+    noButton.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
